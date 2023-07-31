1c2986c22f46f32d1d482563e3728b36a2e848c7
Draculakl07 <131325179+Draculakl07@users.noreply.github.com>
2023-07-31 14:19:20 +1000

    1st update

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-// In-memory data store for messages
+// In-memory data store for messages and user information
 let messages = [];
+let users = {};
 
 // Serve the static files in the "public" folder
 app.use(express.static('public'));
@@ -18,23 +19,33 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // Send all existing messages to the newly connected user
-  socket.emit('messages', messages);
+  // Handle incoming messages and associate them with the user
+  socket.on('sendMessage', (data) => {
+    const { username, message } = data;
+    const userMessage = { username, message };
+    messages.push(userMessage);
+    io.emit('newMessage', userMessage); // Broadcast the new message to all connected users
+  });
 
-  // Handle incoming messages
-  socket.on('sendMessage', (message) => {
-    messages.push(message);
-    io.emit('newMessage', message); // Broadcast the new message to all connected users
+  // Handle new user connections and store their username
+  socket.on('newUser', (username) => {
+    users[socket.id] = username;
+    socket.broadcast.emit('userConnected', username); // Notify other users that a new user has joined
   });
 
   // Handle disconnection
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+    const username = users[socket.id];
+    console.log('User disconnected:', username);
+    if (username) {
+      delete users[socket.id];
+      socket.broadcast.emit('userDisconnected', username); // Notify other users that a user has left
+    }
   });
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
